a658021f7852fa9eaa2f87e981fa9ce226f0c3d9
xaNdEN1337 <novikovvitos1337@gmail.com>
2024-06-27 23:59:45 +0300

    train third

--- new.py
+++ new.py
@@ -13,13 +13,18 @@ try:
     # Курсор для выполнения операций с базой данных
     cursor = connection.cursor()
     # Распечатать сведения о PostgreSQL
-    print("Информация о сервере PostgreSQL")
-    print(connection.get_dsn_parameters(), "\n")
+    # print("Информация о сервере PostgreSQL")
+    # print(connection.get_dsn_parameters(), "\n")
     # Выполнение SQL-запроса
-    cursor.execute("SELECT version();")
-    # Получить результат
-    record = cursor.fetchone()
-    print("Вы подключены к - ", record, "\n")
+    print("Схемы и их таблицы")
+    cursor.execute("select schemaname, tablename from pg_tables \
+                    where schemaname != 'pg_catalog' \
+                    and schemaname !='information_schema' \
+                    and schemaname !='public' \
+                    order by schemaname;")
+    # # Получить результат
+    # record = cursor.fetchone()
+    # print("Вы подключены к - ", record, "\n")
 
 except (Exception, Error) as error:
     print("Ошибка при работе с PostgreSQL", error)
