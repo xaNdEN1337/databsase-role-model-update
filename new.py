db00a7e8b7cb2956d681ee681e841aea7521e3ce
xaNdEN1337 <novikovvitos1337@gmail.com>
2024-06-27 23:43:26 +0300

    train second

--- new.py
+++ new.py
@@ -1,1 +1,30 @@
-print('heLLOS2')
+import psycopg2
+from psycopg2 import Error
+
+try:
+    # Подключение к существующей базе данных
+    connection = psycopg2.connect(user="dbinstaller",
+                                  # пароль, который указали при установке PostgreSQL
+                                  password="dbinstaller",
+                                  host="192.168.0.133",
+                                  port="5432",
+                                  database="uvz")
+
+    # Курсор для выполнения операций с базой данных
+    cursor = connection.cursor()
+    # Распечатать сведения о PostgreSQL
+    print("Информация о сервере PostgreSQL")
+    print(connection.get_dsn_parameters(), "\n")
+    # Выполнение SQL-запроса
+    cursor.execute("SELECT version();")
+    # Получить результат
+    record = cursor.fetchone()
+    print("Вы подключены к - ", record, "\n")
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
